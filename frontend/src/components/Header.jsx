71a2ce950c8394e3d35ff7da7791a87b1222d466
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-12 22:06:10 -0500

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -46,11 +46,11 @@ export default function Header() {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
         <NavLink to="about" className="flex flex-col items-center gap-1">
-          <p>ABOUT</p>
+          <p>Sobre</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
         <NavLink to="contact" className="flex flex-col items-center gap-1">
-          <p>CONTACT</p>
+          <p>Contacto</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
