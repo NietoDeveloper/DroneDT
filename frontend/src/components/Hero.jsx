1a0ad7fa455eaa52aae8508a51f237399cf0145c
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-12 22:45:27 -0500

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
         <div className="text-[#414141]">
           <div className="flex gap-2 items-center">
             <p className="w-11 h-[2px] bg-[#414141]"></p>
-            <p className="font-medium text-sm text-base"> OUR BESTSELLERS</p>
+            <p className="font-medium text-sm text-base"> </p>
           </div>
           <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
