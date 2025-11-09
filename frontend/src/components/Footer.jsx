72db339c15c114774414c9d8037aca6ad983a806
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-08 22:17:33 -0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -31,7 +31,7 @@ export default function Footer() {
           </div>
         </div>
         <div>
-          <p className="text-xl font-medium mb-5"></p>
+          <p className="text-xl font-medium mb-5">Contactanos</p>
           <div className="flex flex-col flex-1 text-gray-600">
             <p className="mb-2">+57 3132151939</p>
             <p className="mb-2">ventas@drondt.com</p>
