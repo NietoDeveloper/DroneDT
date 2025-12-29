29c810ac8c51f6cbc420daa5c6fc287ec6b1a585
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-29 13:47:01 -0500

    Update server.js

--- Base/backend/server.js
+++ Base/backend/server.js
@@ -13,8 +13,6 @@ app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
+
 
 app.listen(port, () => console.log("Server started on port: " + port));
