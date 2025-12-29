74f4f8c050830a0df6ff218a0ca12833ffbe3ad7
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-29 17:45:08 -0500

    Update eslint.config.js

--- Base/frontend/eslint.config.js
+++ Base/frontend/eslint.config.js
@@ -2,17 +2,7 @@ import js from "@eslint/js";
 import globals from "./frontend/node_modules/globals";
 import react from "./frontend/node_modules/eslint-plugin-react";
 import reactHooks from "./frontend/node_modules/eslint-plugin-react-hooks";
-import reactRefresh from "./frontend/node_modules/eslint-plugin-react-refresh";
 
-
-    settings: { react: { version: "18.3" } },
-    plugins: {
-      react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-
-      ],
     },
   },
 ];
