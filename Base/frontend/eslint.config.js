4e3d2b968f6a296d65fe55c6bd7c4ea940f52a1a
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-29 17:45:01 -0500

    Update eslint.config.js

--- Base/frontend/eslint.config.js
+++ Base/frontend/eslint.config.js
@@ -11,15 +11,7 @@ import reactRefresh from "./frontend/node_modules/eslint-plugin-react-refresh";
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      ...reactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+
       ],
     },
   },
