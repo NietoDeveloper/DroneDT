2e5c2e3a48950c30254f13c4c461aeefcb17dbc4
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-29 17:45:14 -0500

    Update eslint.config.js

--- Base/frontend/eslint.config.js
+++
@@ -1,8 +0,0 @@
-import js from "@eslint/js";
-import globals from "./frontend/node_modules/globals";
-import react from "./frontend/node_modules/eslint-plugin-react";
-import reactHooks from "./frontend/node_modules/eslint-plugin-react-hooks";
-
-    },
-  },
-];
