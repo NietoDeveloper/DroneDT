3f2e500075c0008ce119306384655e80259d9374
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:53:15 -0500

    Update Header.jsx

--- Base/frontend/src/components/Header.jsx
+++ Base/frontend/src/components/Header.jsx
@@ -2,23 +2,6 @@ import { Link, NavLink, useLocation } from "react-router-dom";
 import clothixLogo from "../assets/clothixLogo.svg";
 import search from "../asol gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                 <p classNa
-      {/* Mobile Menu */}
-      <div
-        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white ease-in duration-300 ${
-          visible ? "w-full" : "w-0"
-        }`}
-      >
-        <div className="flex flex-col text-gray-600">
-          <div
-            className="flex cursor-pointer gap-4 items-center p-3"
-            onClick={() => setVisible(false)}
-          >
-            <img className="h-4 rotate-180" src={backArrow} alt="" />
-            <p>Atras</p>
-          </div>
-          <NavLink
-            className="py-2 pl-6 border"
-            onClick={() => setVisible(false)}
             to="/"
           >
             Inicio
