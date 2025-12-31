fc55003f1c78f9a97cf11bf7c0076d2eafbb5590
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:19:28 -0500

    Update Footer.jsx

--- Base/frontend/src/components/Footer.jsx
+++ Base/frontend/src/components/Footer.jsx
@@ -11,11 +11,6 @@ export default function Footer() {
             Construimos Drones Colombianos
           </p>
         </div>
-                <div>
-          <p className="text-xl font-medium mb-5">Contactanos</p>
-          <div className="flex flex-col flex-1 text-gray-600">
-            <p className="mb-2">+57 3132151939</p>
-            <p className="mb-2">ventas@drondt.com</p>
           </div>
         </div>
         <div>
