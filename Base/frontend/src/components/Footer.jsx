660ce96a77b6164f582f6627011a9922ebe0c676
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:19:44 -0500

    Update Footer.jsx

--- Base/frontend/src/components/Footer.jsx
+++ Base/frontend/src/components/Footer.jsx
@@ -2,12 +2,7 @@ import { Link } from "react-router-dom";
 import clothixLogo from "../assets/clothixLogo.svg";
 
 export default function Footer() {
-  return (
-    <div>
-            <Link to="contact" className="mb-2 w-fit">
-              Contactanos
-            </Link>
-            <p className="mb-2">Envio</p>
+  return (>
             <p className="mb-2">Politica De Privacidad</p>
           </div>
         </div>
