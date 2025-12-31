174c146e39a1defb0f055cdb391c1297cf131710
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:53:28 -0500

    Update Header.jsx

--- Base/frontend/src/components/Header.jsx
+++ Base/frontend/src/components/Header.jsx
@@ -1,20 +1,5 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
-import clothixLogo from "../assets/clothixLogo.svg";
-import search from "..
-          <NavLink
-            className="py-2 pl-6 border"
-            onClick={() => setVisible(false)}
-            to="about"
-          >
-            Sobre
-          </NavLink>
-          <NavLink
-            className="py-2 pl-6 border"
-            onClick={() => setVisible(false)}
-            to="contact"
-          >
-            Contacto
-          </NavLink>
+import clothixLogo from
         </div>
       </div>
     </nav>
