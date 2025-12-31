ea736bd57ac5107672993994bea0c23bc1c9c4d3
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:10:24 -0500

    Update CartTotal.jsx

--- Base/frontend/src/components/CartTotal.jsx
+++ Base/frontend/src/components/CartTotal.jsx
@@ -1,26 +1,6 @@
 import { useContext } from "react";
 import Title from "./Title";
-import { ShopContext } from "../Context/ShopContext";
-
-export default function CartTotal() {
-  const { currency, deliveryFee, getCartAmount } = useContext(ShopContext);
-  const subtotal = getCartAmount();
-
-  return (
-    <>
-      <div className="text-2xl">
-        <Title title1="CART" title2="TOTAL" />
-      </div>
-      <div className="flex flex-col gap-2 mt-2 text-sm">
-        <div className="flex justify-between">
-          <p>Subtotal</p>
-          <p>
-            {currency}
-            {subtotal}.00
-          </p>
-        </div>
-        <hr />
-        <div className="flex justify-between">
+imx justify-between">
           <p>Shipping Fee</p>
           <p>
             {currency}
