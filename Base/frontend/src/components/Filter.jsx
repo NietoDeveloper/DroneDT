132da9125b5ac23324080a8d3eacdc620262398e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:04:16 -0500

    Update Filter.jsx

--- Base/frontend/src/components/Filter.jsx
+++ Base/frontend/src/components/Filter.jsx
@@ -1,25 +1,7 @@
 import { useStat
           <p className="flex gap-2">
             <input
-              className="w-3"
-              type="checkbox"
-              value="Kids"
-         
-              onChange={toggleSubCategory}
-            />
-            Topwear
-          </p>
-          <p className="flex gap-2">
-            <input
-              className="w-3"
-              type="checkbox"
-              value="Bottomwear"
-              onChange={toggleSubCategory}
-            />
-            Bottomwear
-          </p>
-          <p className="flex gap-2">
-            <input
+              className="
               className="w-3"
               type="checkbox"
               value="Winterwear"
