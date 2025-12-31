3aed26bbb9352297eede6a0425d2e86f136d7473
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:55:18 -0500

    Update LatestCollections.jsx

--- Base/frontend/src/components/LatestCollections.jsx
+++ Base/frontend/src/components/LatestCollections.jsx
@@ -5,9 +5,6 @@ import Title from "./Title";
 
   return (src={dron1} alt="" />
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {productElements}
-      </div>
     </div>
   );
 }
