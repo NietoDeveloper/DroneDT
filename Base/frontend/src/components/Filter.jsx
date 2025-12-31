273bccf1c7f9c3b6cc3ddacb5fdb21e042ca3e1c
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:04:11 -0500

    Update Filter.jsx

--- Base/frontend/src/components/Filter.jsx
+++ Base/frontend/src/components/Filter.jsx
@@ -4,24 +4,7 @@ import { useStat
               className="w-3"
               type="checkbox"
               value="Kids"
-              onChange={toggleCategory}
-            />
-            Kids
-          </p>
-        </div>
-      </div>
-      <div
-        className={`border border-gray-300 pl-5 py-5 my-5 ${
-          showFilter ? "" : "hidden"
-        } sm:block`}
-      >
-        <p className="mb-3 font-medium">TYPE</p>
-        <div className="flex flex-col gap-3 font-light text-gray-700">
-          <p className="flex gap-2">
-            <input
-              className="w-3"
-              type="checkbox"
-              value="Topwear"
+         
               onChange={toggleSubCategory}
             />
             Topwear
