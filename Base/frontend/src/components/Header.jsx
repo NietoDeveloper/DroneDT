2a1cea28c28636642c96a2650d4ba5e48ae58056
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:53:08 -0500

    Update Header.jsx

--- Base/frontend/src/components/Header.jsx
+++ Base/frontend/src/components/Header.jsx
@@ -1,24 +1,7 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
 import clothixLogo from "../assets/clothixLogo.svg";
 import search from "../asol gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-                <p className="cursor-pointer hover:text-black">Mi Perfil</p>
-                <p
-                  onClick={() => navigate("orders")}
-                  className="cursor-pointer hover:text-black"
-                >
-                  Order
-           /p>
-              </div>
-            </div>
-          )}
-        </div>
-        <Link to="carnter w-5 sm:hidden"
-          onClick={() => setVisible(true)}
-          src={menu}
-          alt=""
-        />
-      </div>
-
+                <p classNa
       {/* Mobile Menu */}
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white ease-in duration-300 ${
