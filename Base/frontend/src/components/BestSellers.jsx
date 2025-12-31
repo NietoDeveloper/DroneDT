da9b5d9038e58328b4e3301856315c20c0f683b6
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:03:06 -0500

    Update BestSellers.jsx

--- Base/frontend/src/components/BestSellers.jsx
+++ Base/frontend/src/components/BestSellers.jsx
@@ -10,23 +10,7 @@ export default function BestSellers() {
             title2="MÁS VENDIDO"
             caption="Descubre el CITY 1.1, nuestro líder en ventas."
           />
-        </div>
-        <div className="flex justify-center w-full mb-8">
-          <a 
-            href="#" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 cursor-pointer transition duration-300 hover:scale-[1.03] hover:shadow-2xl rounded-xl overflow-hidden block"
-          >
-            <img 
-              className="w-full h-auto object-cover" 
-              src={agro3} 
-              alt="Drone CITY 1.1" 
-            />
-          </a>
-        </div>
-        <div className="space-y-2 mt-4">
-          <h2 className="text-3xl sm:text-4xl font-extrabold text-blue-700">
+   l font-extrabold text-blue-700">
             DRON CITY 1.1
           </h2>
           <h3 className="text-xl sm:text-2xl font-semibold text-gray-600">
