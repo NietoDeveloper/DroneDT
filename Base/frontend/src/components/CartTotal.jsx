4fe0688244ab8c222d973df5b1f93a8f13331c81
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:11:15 -0500

    Update CartTotal.jsx

--- Base/frontend/src/components/CartTotal.jsx
+++ Base/frontend/src/components/CartTotal.jsx
@@ -1,10 +1,2 @@
 import { useContext } from "react";
 import Title from "./Title";
-imx justify-between">
-          <p>Shipping Fee</p>
-          <p>
-            {currency}
-            {deliveryFee}.00
-          </p>
-        </div>
-     
