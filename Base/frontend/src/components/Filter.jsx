4c111a6528e9722baf8caf1f863201948dcce56c
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:04:21 -0500

    Update Filter.jsx

--- Base/frontend/src/components/Filter.jsx
+++ Base/frontend/src/components/Filter.jsx
@@ -1,16 +1,2 @@
 import { useStat
-          <p className="flex gap-2">
-            <input
-              className="
-              className="w-3"
-              type="checkbox"
-              value="Winterwear"
-              onChange={toggleSubCategory}
-            />
-            Winterwear
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
+          <
