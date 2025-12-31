1e1e658093bc78eecda3cc3f0db4c97b75f25785
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:09:01 -0500

    Update Hero.jsx

--- Base/frontend/src/components/Hero.jsx
+++ Base/frontend/src/components/Hero.jsx
@@ -5,11 +5,5 @@ export default function Hero() {
     <div className="flex flex-col sm:flex-row border border-gray-400">
       <div className="py-10 sm:w-1/2 flex flex-col justify-center items-center">
         <div className="text-[#414141]">
-          <div cl14141]"></p>
-          </div>
-        </div>
-      </div>
-      <img className="sm:w-1/2" src={homepageHero} alt="" />
-    </div>
-  );
+       
 }
