c71415475996f8efe3f3500be4b499587c94de54
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:19:50 -0500

    Update Footer.jsx

--- Base/frontend/src/components/Footer.jsx
+++ Base/frontend/src/components/Footer.jsx
@@ -1,14 +1,6 @@
 import { Link } from "react-router-dom";
 import clothixLogo from "../assets/clothixLogo.svg";
-
-export default function Footer() {
-  return (>
-            <p className="mb-2">Politica De Privacidad</p>
-          </div>
-        </div>
-      </div>
-      <hr />
-      <p className="py-5 text-sm text-center">2025 Created by NietoDeveloper ©</p>
+d by NietoDeveloper ©</p>
     </div>
   );
 }
