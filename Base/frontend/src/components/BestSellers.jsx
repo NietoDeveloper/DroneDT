8f6c86c115c4c499a5b924f126b1807a9b451908
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:03:19 -0500

    Update BestSellers.jsx

--- Base/frontend/src/components/BestSellers.jsx
+++ Base/frontend/src/components/BestSellers.jsx
@@ -1,11 +1,4 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import Title from "./Title";
-import agro3 from "../assets/agro3.png";
-
-export default function BestSellers() {
-  const { products } = useContext(ShopContext);
-
-  
-  );
-}
+i
