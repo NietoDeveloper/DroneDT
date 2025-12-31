adb84ffda31c15072f6f18efd7e0fce12004e3ef
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-30 23:09:58 -0500

    Update ProductList.jsx

--- Base/frontend/src/components/ProductList.jsx
+++ Base/frontend/src/components/ProductList.jsx
@@ -2,16 +2,6 @@
   return (
     <div className="flex-1">
 
-      <div className="flex justify-between text-base sm:text-xl lg:text-2xl mb-4">
-        <Title title1="ALL" title2="COLLECTIONS" />
-        <select
-          onChange={sortFilter}
-          v
-      </div>
-
-      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 gap-y-6">
-        {productElements}
-      </div>
-    </div>
+      <div className="flex justify-b
   );
 }
