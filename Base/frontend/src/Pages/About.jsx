427bec92a65ef2e4958a7275f27f0eace0fabc6a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 01:35:15 -0500

    Update About.jsx

--- Base/frontend/src/Pages/About.jsx
+++ Base/frontend/src/Pages/About.jsx
@@ -8,14 +8,7 @@ export default function About() {
 
       <div className="text-xl py-4">
         <Title title1="WHY" title2="CHOOSE US?" />
-        <div className="flex flex-col md:flex-row text-sm mb-20 pt-4">
-          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
-            <p className="font-bold">Quality Assurance:</p>
-            <p className="text-gray-600">
-              We meticulously select and vet each product to ensure it meets our
-              stringent quality standards.
-            </p>
-          </div>
+
           <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
             <p className="font-bold">Convenience:</p>
             <p className="text-gray-600">
