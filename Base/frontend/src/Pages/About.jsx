20c18c91924d8aec69e6fefaa2a0fb7280aa3d2d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 01:35:43 -0500

    Update About.jsx

--- Base/frontend/src/Pages/About.jsx
+++ Base/frontend/src/Pages/About.jsx
@@ -1,8 +1,1 @@
 import Subscription from "../components/Subscription";
-import Title from "../components/Title";
-import aboutHero from "../assets/about_img.png";
-
-export default function About() {
-  return (
-    <>
-
