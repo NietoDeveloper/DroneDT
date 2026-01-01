5fe4265eb68b849083fa65e4cc64b444f4bcb58a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 00:45:26 -0500

    Update Collection.jsx

--- Base/frontend/src/Pages/Collection.jsx
+++ Base/frontend/src/Pages/Collection.jsx
@@ -7,8 +7,6 @@ export default function Collection() {
   const [subCategory, setSubCategory] = useState([]);
 
   return (
-    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10">
 
-    </div>
   );
 }
