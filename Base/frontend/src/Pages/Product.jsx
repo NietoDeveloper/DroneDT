98409b2ee77c57ff224b2769a198a497f3cd1732
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-31 21:12:30 -0500

    Update Product.jsx

--- Base/frontend/src/Pages/Product.jsx
+++ Base/frontend/src/Pages/Product.jsx
@@ -18,12 +18,6 @@ export default function Product() {
 
         <div className="flex-1">
 
-          <button
-            onClick={() => addToCart(productData._id, size)}
-            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
-          >
-            ADD TO CART
-          </button>
           <hr className="mt-8 sm:w-4/5" />
           <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
             <p>100% Original product.</p>
