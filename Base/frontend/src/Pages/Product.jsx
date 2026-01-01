d05e47806ed42ae79d67d285d1c8b6a9f33e309a
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-31 21:13:58 -0500

    Update Product.jsx

--- Base/frontend/src/Pages/Product.jsx
+++ Base/frontend/src/Pages/Product.jsx
@@ -3,5 +3,4 @@ import { useLocation, useParams } from "react-router-dom";
 import { ShopContext } from "../Context/ShopContext";
 import star from "../assets/star_icon.png";
 import noStar from "../assets/star_dull_icon.png";
-import RelatedProducts from "../components/RelatedProducts";
 
