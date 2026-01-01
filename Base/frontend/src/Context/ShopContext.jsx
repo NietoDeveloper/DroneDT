5ebd0d0f78c4e284d1c01960504c2b07ec09e680
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-31 19:48:43 -0500

    Update ShopContext.jsx

--- Base/frontend/src/Context/ShopContext.jsx
+++ Base/frontend/src/Context/ShopContext.jsx
@@ -19,9 +19,6 @@ export default function ShopContextProvider(props) {
 
   async function addToCart(productId, size) {
 
-  function getCartCount() {
-
-  }
 
   async function updateQuantity(productId, size, quantity) {
     let cartData = structuredClone(cartItems);
