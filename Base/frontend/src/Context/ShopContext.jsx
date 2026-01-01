72061aff0e2842c2b9773587e7d7baff02b78a2a
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-31 20:20:59 -0500

    Update ShopContext.jsx

--- Base/frontend/src/Context/ShopContext.jsx
+++ Base/frontend/src/Context/ShopContext.jsx
@@ -2,5 +2,4 @@ import { createContext, useEffect, useState } from "react";
 // import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
