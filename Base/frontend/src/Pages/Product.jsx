3ccc289798dcd9681803fad03181fcfe0be46147
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-31 20:55:02 -0500

    Update Product.jsx

--- Base/frontend/src/Pages/Product.jsx
+++ Base/frontend/src/Pages/Product.jsx
@@ -16,13 +16,7 @@ export default function Product() {
 
   return productData ? (
     <div className="pt-10 transition-opacity ease-in duration-500 opacity-100">
-      {/* Product Data */}
-      <div className="flex gap-12 flex-col sm:flex-row">
-        {/* Product Images */}
-        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
-            {productData.image.map((img, index) => (
-              <img
+      {
                 key={index}
                 className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
                 onClick={() => setImage(img)}
