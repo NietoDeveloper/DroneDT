ba84b6eb28e0f2e996ed3ebd26a5e88bd23764b3
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-31 21:11:27 -0500

    Update Product.jsx

--- Base/frontend/src/Pages/Product.jsx
+++ Base/frontend/src/Pages/Product.jsx
@@ -17,11 +17,6 @@ export default function Product() {
   return productData ? (
 
         <div className="flex-1">
-
-
-          <p className="mt-5 text-gray-500 md:w-4/5">
-            {productData.description}
-          </p>
           <div className="flex flex-col gap-4 my-8">
             <p>Select Size</p>
             <div className="flex gap-2">
