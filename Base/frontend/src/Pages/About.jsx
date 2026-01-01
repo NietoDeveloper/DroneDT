6788903868855386252c902c18157cf3045bc65e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 01:35:31 -0500

    Update About.jsx

--- Base/frontend/src/Pages/About.jsx
+++ Base/frontend/src/Pages/About.jsx
@@ -6,20 +6,7 @@ export default function About() {
   return (
     <>
 
-      <div className="text-xl py-4">
-        <Title title1="WHY" title2="CHOOSE US?" />
 
-          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
-            <p className="font-bold">Convenience:</p>
-            <p className="text-gray-600">
-              With our user-friendly interface and hassle-free ordering process,
-              shopping has never been easier.
-            </p>
-          </div>
-          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
-            <p className="font-bold">Exceptional Customer Service:</p>
- 
-          </div>
         </div>
       </div>
       <Subscription />
