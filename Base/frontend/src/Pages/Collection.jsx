187018a1c24371964f48ecde984e0464754f6f8d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 00:45:07 -0500

    Update Collection.jsx

--- Base/frontend/src/Pages/Collection.jsx
+++ Base/frontend/src/Pages/Collection.jsx
@@ -8,8 +8,7 @@ export default function Collection() {
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10">
-      {/* Left side */}
-      <Filter
+
 
 
       <ProductList category={category} subCategory={subCategory} />
