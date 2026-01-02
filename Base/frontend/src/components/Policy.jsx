856450319fb76449ab87d51890cd8dd8e2d14332
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:34:53 -0500

    Update Policy.jsx

--- Base/frontend/src/components/Policy.jsx
+++ Base/frontend/src/components/Policy.jsx
@@ -1,7 +1,3 @@
 import exchange from "../assets/exchange_icon.png";
 import quality from "../assets/quality_icon.png";
 import support from "../assets/support_img.png";
-
-export default function Policy() {
-
-}
