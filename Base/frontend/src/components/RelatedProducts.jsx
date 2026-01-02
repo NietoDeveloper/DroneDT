02499059c5df265313ec436a1406d3d1b3d9de85
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:45:44 -0500

    Update RelatedProducts.jsx

--- Base/frontend/src/components/RelatedProducts.jsx
+++ Base/frontend/src/components/RelatedProducts.jsx
@@ -2,7 +2,3 @@ import { useContext, useEffect, useState } from "react";
 import Title from "./Title";
 import { ShopContext } from "../Context/ShopContext";
 import ProductCard from "./ProductCard";
-
-export default function RelatedProducts({ category, subCategory }) {
-
-}
