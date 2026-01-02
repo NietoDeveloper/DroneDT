47cad090b974349559aae8a6bb27f98732186754
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:30:57 -0500

    Update Layout.jsx

--- Base/frontend/src/components/Layout.jsx
+++ Base/frontend/src/components/Layout.jsx
@@ -2,5 +2,4 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
-import { useEffect } from "react";
 
