0dead54690f9dfe03e582a4dbf8f409fc2b7ac58
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:34:21 -0500

    Update Policy.jsx

--- Base/frontend/src/components/Policy.jsx
+++ Base/frontend/src/components/Policy.jsx
@@ -7,10 +7,6 @@ export default function Policy() {
     <div className="min-h-screen flex flex-col items-center justify-center p-4 sm:p-8 bg-white text-gray-700">
 
       <div className="w-full max-w-6xl flex flex-col md:flex-row justify-around gap-10 md:gap-8 py-10 md:py-0 text-center">
- 
-        <div className="flex flex-col items-center p-4 md:p-6 rounded-lg transition duration-300 hover:shadow-xl hover:scale-[1.02]">
-        
-        </div>
         
       </div>
       
