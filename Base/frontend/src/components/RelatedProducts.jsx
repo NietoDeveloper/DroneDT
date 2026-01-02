f43366aca57499c4c2e266318f9ea1589eb7b77f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:45:49 -0500

    Update RelatedProducts.jsx

--- Base/frontend/src/components/RelatedProducts.jsx
+++ Base/frontend/src/components/RelatedProducts.jsx
@@ -1,4 +1,2 @@
 import { useContext, useEffect, useState } from "react";
-import Title from "./Title";
-import { ShopContext } from "../Context/ShopContext";
-import ProductCard from "./ProductCard";
+ ProductCard from "./ProductCard";
