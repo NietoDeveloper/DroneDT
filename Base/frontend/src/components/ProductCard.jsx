c47b1d5be7bba6463e27edf8880b0711114b4a45
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:40:19 -0500

    Update ProductCard.jsx

--- Base/frontend/src/components/ProductCard.jsx
+++ Base/frontend/src/components/ProductCard.jsx
@@ -4,10 +4,6 @@ import { ShopContext } from "../Context/ShopContext";
 
 export default function ProductCard({ id, image, name, price }) {
   const { currency } = useContext(ShopContext);
-  return (
-    <Link to={`/product/${id}`}>
-      <div className="overflow-hidden h-[300px] border rounded-lg p-4 shadow-sm text-gray-700">
-
-
+  return (s
   );
 }
