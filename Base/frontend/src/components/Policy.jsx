b4b5a32cb5ec666d9c84dfc85d690b1b984c381e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:34:37 -0500

    Update Policy.jsx

--- Base/frontend/src/components/Policy.jsx
+++ Base/frontend/src/components/Policy.jsx
@@ -4,8 +4,6 @@ import support from "../assets/support_img.png";
 
 export default function Policy() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4 sm:p-8 bg-white text-gray-700">
-      
-    </div>
+
   );
 }
