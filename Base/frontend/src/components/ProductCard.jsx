441b88cd7e43aeb022de98de55c0ef209e5bed13
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:40:10 -0500

    Update ProductCard.jsx

--- Base/frontend/src/components/ProductCard.jsx
+++ Base/frontend/src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ export default function ProductCard({ id, image, name, price }) {
   return (
     <Link to={`/product/${id}`}>
       <div className="overflow-hidden h-[300px] border rounded-lg p-4 shadow-sm text-gray-700">
-        <img
+
 
   );
 }
