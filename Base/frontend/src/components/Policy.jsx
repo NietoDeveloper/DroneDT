c79d3a5fbad5be8ca3cc8bf0992ae708e93ae831
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-01 21:34:30 -0500

    Update Policy.jsx

--- Base/frontend/src/components/Policy.jsx
+++ Base/frontend/src/components/Policy.jsx
@@ -5,10 +5,6 @@ import support from "../assets/support_img.png";
 export default function Policy() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4 sm:p-8 bg-white text-gray-700">
-
-      <div className="w-full max-w-6xl flex flex-col md:flex-row justify-around gap-10 md:gap-8 py-10 md:py-0 text-center">
-        
-      </div>
       
     </div>
   );
