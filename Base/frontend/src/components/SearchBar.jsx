4f5526d7f4ecc8ed37cf44ade84a47e82a8afb3a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-02 21:08:12 -0500

    Update SearchBar.jsx

--- Base/frontend/src/components/SearchBar.jsx
+++ Base/frontend/src/components/SearchBar.jsx
@@ -23,12 +23,7 @@ export default function SearchBar() {
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <img className="w-4" src={searchIcon} alt="" />
       </div>
-      <img
-        onClick={() => setShowSearch(false)}
-
-        src={exitIcon}
-        alt=""
-      />
+      
     </div>
   ) : null;
 }
