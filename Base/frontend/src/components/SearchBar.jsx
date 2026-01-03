d06060e3cdff2c673674233ad348e4305f4ca896
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-02 21:08:52 -0500

    Update SearchBar.jsx

--- Base/frontend/src/components/SearchBar.jsx
+++ Base/frontend/src/components/SearchBar.jsx
@@ -1,7 +1,3 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import searchIcon from "../assets/search_icon.png";
-import exitIcon from "../assets/cross_icon.png";
-import { useLocation } from "react-router-dom";
-
-export default function SearchBar() {
