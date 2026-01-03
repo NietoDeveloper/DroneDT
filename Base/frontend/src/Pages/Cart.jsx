cd36d73d5012a6a80af6d809ccd489bc9d9b4aac
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-02 21:06:56 -0500

    Update Cart.jsx

--- Base/frontend/src/Pages/Cart.jsx
+++ Base/frontend/src/Pages/Cart.jsx
@@ -1,8 +1,4 @@
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { useContext, useEffect, useState } from "react";
-import { ShopContext } from "../Context/ShopContext";
-import bin from "../assets/bin_icon.png";
-
-export default function Cart() {
 
