8c6ff843007bdfeb9d1fe2599b11bfa03f6b792c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-02 21:56:53 -0500

    Update Subscription.jsx

--- Base/frontend/src/components/Subscription.jsx
+++ Base/frontend/src/components/Subscription.jsx
@@ -3,4 +3,4 @@ import React, { useState } from 'react';
 const Subscription = () => {
   const [email, setEmail] = useState('');
   const [isSubscribed, setIsSubscribed] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading,
