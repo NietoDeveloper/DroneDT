85fb264c1ecd5aec7b4801a5c0184488c3e81797
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-02 21:53:19 -0500

    Update Subscription.jsx

--- Base/frontend/src/components/Subscription.jsx
+++ Base/frontend/src/components/Subscription.jsx
@@ -34,7 +34,7 @@ const Subscription = () => {
         </div>
 
         <form onSubmit={handleSubmit} className="mt-8">
-          {isSubscribed ? (en-700 rounded-lg text-sm font-medium text-center transition-opacity duration-500">
+          {isSubscribed ? (en-70m text-center transition-opacity duration-500">
 
             </div>
           )
