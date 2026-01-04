146dddc2012c3e2a40712ddd79f040b0f90860dd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-03 20:38:58 -0500

    Update Orders.jsx

--- Base/frontend/src/Pages/Orders.jsx
+++ Base/frontend/src/Pages/Orders.jsx
@@ -1,4 +1,2 @@
 import { useContext, useEffect, useState } from "react";
-import { ShopContext } from "../Context/ShopContext";
-import Title from "../components/Title";
-import axios from "axios";
+port axios from "axios";
