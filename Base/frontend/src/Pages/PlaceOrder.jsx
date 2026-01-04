cc382f54bd7e81cb54b4d95be45e611032f787fb
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-03 20:49:15 -0500

    Update PlaceOrder.jsx

--- Base/frontend/src/Pages/PlaceOrder.jsx
+++ Base/frontend/src/Pages/PlaceOrder.jsx
@@ -25,7 +25,7 @@ import paypalLogo from "../assets/paypal_logo.png";
 
   return (
     <form
-      onSubmit={onSubmitHandler}
+
       className="flex flex-col sm:flex-row justify-between pt-5 sm:pt-14 min-h-[80vh]"
     >
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
