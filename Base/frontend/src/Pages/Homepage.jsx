505224cd953a389b352e224032837aecd65923dc
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 18:14:46 -0500

    Update Homepage.jsx

--- Base/frontend/src/Pages/Homepage.jsx
+++ Base/frontend/src/Pages/Homepage.jsx
@@ -2,7 +2,6 @@ import BestSellers from "../components/BestSellers";
 import Hero from "../components/Hero";
 import LatestCollections from "../components/LatestCollections";
 import Policy from "../components/Policy";
-import Subscription from "../components/Subscription";
 
 export default function Homepage() {
   return (
