3ba283f7ea9d6586a4d0e162049126e27904f810
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 18:56:19 -0500

    Update Contact.jsx

--- Base/frontend/src/Pages/Contact.jsx
+++ Base/frontend/src/Pages/Contact.jsx
@@ -13,9 +13,7 @@ export default function Contact() {
 
         <div className="flex flex-col justify-center gap-6 text-gray-600 items-start">
 
- 
-            <br />
-          </p>
+
 
           <p className="text-gray-500">
 
