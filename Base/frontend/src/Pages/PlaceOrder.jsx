2ff936ac0f7fbeae6acae69d49503dba36e53903
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-03 20:50:44 -0500

    Update PlaceOrder.jsx

--- Base/frontend/src/Pages/PlaceOrder.jsx
+++ Base/frontend/src/Pages/PlaceOrder.jsx
@@ -3,24 +3,6 @@ import Title from "../components/Title";
 import paypalLogo from "../assets/paypal_logo.png";
 
 
-  const {
-
-
-  } = useContext(ShopContext);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    state: "",
-
-  });
-
-  function onChangeHandler(e) {
-
-
-    setFormData((data) => ({ ...data, [name]: value }));
-  }
-
-  function demoMethod(method) {
-"flex items-center gap-3 border p-2 px-3 cursor-pointer"
             >
 
           </div>
