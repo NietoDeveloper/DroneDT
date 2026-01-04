1676b5eada4f000ed2a2e9a922b2a8e2f8265675
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 18:14:55 -0500

    Update Homepage.jsx

--- Base/frontend/src/Pages/Homepage.jsx
+++ Base/frontend/src/Pages/Homepage.jsx
@@ -1,8 +1,6 @@
 import BestSellers from "../components/BestSellers";
 import Hero from "../components/Hero";
 import LatestCollections from "../components/LatestCollections";
-import Policy from "../components/Policy";
-
 export default function Homepage() {
   return (
     <>
