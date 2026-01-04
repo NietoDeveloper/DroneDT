d012b02b41324ed521d3963bbaaae57f87490611
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-03 20:38:52 -0500

    Update Orders.jsx

--- Base/frontend/src/Pages/Orders.jsx
+++ Base/frontend/src/Pages/Orders.jsx
@@ -2,7 +2,3 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import Title from "../components/Title";
 import axios from "axios";
-
-export default function Orders() {
-  const { backendUrl, token, currency } = useContext(ShopContext);
- 
