4c366188a32fc70a2148c818037d35186197eba7
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 18:57:01 -0500

    Update Contact.jsx

--- Base/frontend/src/Pages/Contact.jsx
+++ Base/frontend/src/Pages/Contact.jsx
@@ -1,6 +1,5 @@
 import Title from "../components/Title";
-import contactHero from "../assets/contact_img.png";
-import Subscription from "../components/Subscription";
+
 
 export default function Contact() {
   return (
