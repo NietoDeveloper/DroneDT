1442fed187c3ed095893a73d52180ba8cf83d318
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 20:40:38 -0500

    Update App.jsx

--- Base/frontend/src/App.jsx
+++ Base/frontend/src/App.jsx
@@ -1,6 +1,2 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Collection from "./Pages/Collection";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Layout from "./components/Layout";
-import Homepage from "./Pages/Homepage";
