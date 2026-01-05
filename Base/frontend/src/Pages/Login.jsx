5817278b1594f9a8ad10af6481cfc201a4df01a9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 20:04:18 -0500

    Update Login.jsx

--- Base/frontend/src/Pages/Login.jsx
+++ Base/frontend/src/Pages/Login.jsx
@@ -2,9 +2,3 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import axios from "axios";
 import { toast } from "react-toastify";
-
-export default function Login() {
-  const [currentState, setCurrentState] = useState("Login");
-  const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
-
-
