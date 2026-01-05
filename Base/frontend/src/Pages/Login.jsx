73f874132457a38d45b9a22f73495eeaf08bed4a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 20:04:29 -0500

    Update Login.jsx

--- Base/frontend/src/Pages/Login.jsx
+++ Base/frontend/src/Pages/Login.jsx
@@ -1,4 +1,3 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
-import axios from "axios";
-import { toast } from "react-toastify";
+
