57400cb1956e90677b2cb0e85557e8f9285e1a0e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 19:46:11 -0500

    Update Login.jsx

--- Base/frontend/src/Pages/Login.jsx
+++ Base/frontend/src/Pages/Login.jsx
@@ -60,12 +60,6 @@ export default function Login() {
 
         className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
       >
-        <div className="inline-flex items-center gap-2 mb-2 mt-10">
-          <p className="prata-regular text-3xl">{currentState}</p>
-        </div>
-        {currentState === "Sign Up" && (
-
-        )}
 
 
         <div className="w-full flex justify-between text-sm mt-[-8px]">
