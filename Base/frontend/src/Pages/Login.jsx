19fba48f42142998f05cb49d9a20d0b61670074b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 19:44:51 -0500

    Update Login.jsx

--- Base/frontend/src/Pages/Login.jsx
+++ Base/frontend/src/Pages/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
   return (
     <div>
       <form
-        onSubmit={formSubmit}
+
         className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
       >
         <div className="inline-flex items-center gap-2 mb-2 mt-10">
