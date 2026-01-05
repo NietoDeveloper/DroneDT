940700be93a3ecfa0e0592e9dbf6056381bcf165
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 20:04:11 -0500

    Update Login.jsx

--- Base/frontend/src/Pages/Login.jsx
+++ Base/frontend/src/Pages/Login.jsx
@@ -7,11 +7,4 @@ export default function Login() {
   const [currentState, setCurrentState] = useState("Login");
   const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("tester@email.com");
-  const [password, setPassword] = useState("tester123");
-
-  async function formSubmit(e) {
-    e.preventDefault();
-    try {
 
