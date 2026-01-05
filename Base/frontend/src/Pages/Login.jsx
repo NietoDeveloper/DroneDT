eb5c5d962efeee6c1d7e5deac278ebfb7eb731ca
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-04 19:44:44 -0500

    Update Login.jsx

--- Base/frontend/src/Pages/Login.jsx
+++ Base/frontend/src/Pages/Login.jsx
@@ -66,7 +66,7 @@ export default function Login() {
         {currentState === "Sign Up" && (
           <input
             type="text"
-            onChange={(e) => setName(e.target.value)}
+
             value={name}
             className="w-full px-3 py-2 border border-gray-800"
             placeholder="Name"
