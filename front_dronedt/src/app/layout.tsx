f4e390e3145b5d15768c6f8461cfa0086682b22f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-05 23:24:17 -0500

    Update layout.tsx

--- front_dronedt/src/app/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next";
-import { GeistSans, GeistMono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar"; // Usando el alias @/ definido en tu identidad técnica
-import Footer from "@/components/Footer";
-
