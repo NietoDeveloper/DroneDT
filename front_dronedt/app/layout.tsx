7e557a6699eb4ffe3042e58281b38f503ab64834
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-06 01:23:35 -0500

    Update layout.tsx

--- front_dronedt/app/layout.tsx
+++ front_dronedt/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans, GeistMono } from "next/font/google";
 import "@/styles/globals.css"; 
-import { Navbar } from "@/components/shared/navbar/Navbar";
+import { Navbar } from "@/components/layout/navbar/Navbar";
 
 // Configuramos las fuentes fuera del componente para evitar re-renders innecesarios
-const geistSans = Geist({
+const geistSans = GeistSans({
   subsets: ["latin"],
   variable: "--font-geist-sans",
   display: "swap",
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   subsets: ["latin"],
   variable: "--font-geist-mono",
   display: "swap",
