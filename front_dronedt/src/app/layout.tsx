d3a05f3d06fc6ba2a592a2b34973596c4b99ed8f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-05 23:24:11 -0500

    Update layout.tsx

--- front_dronedt/src/app/layout.tsx
+++ front_dronedt/src/app/layout.tsx
@@ -4,15 +4,3 @@ import "./globals.css";
 import Navbar from "@/components/Navbar"; // Usando el alias @/ definido en tu identidad técnica
 import Footer from "@/components/Footer";
 
-const geistSans = GeistSans({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "DroneDT",
