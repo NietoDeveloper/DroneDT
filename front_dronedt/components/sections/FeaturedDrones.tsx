32871f65c1f3eda094ff8bcc3ab866602365bb5e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-08 23:54:54 -0500

    Update FeaturedDrones.tsx

--- front_dronedt/components/sections/FeaturedDrones.tsx
+++ front_dronedt/components/sections/FeaturedDrones.tsx
@@ -36,7 +36,7 @@ export default function FeaturesDrones() {
        
      
 
-                    <p className="text-gray-
+             
                   <span className="text-xl font-bold text-yellowColor">{drone.price}</span>
                 </div>
 
