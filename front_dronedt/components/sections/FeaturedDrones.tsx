5e0c43bf675333d0bbaee5307ad8fd8a8837759f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-08 23:53:35 -0500

    Update FeaturedDrones.tsx

--- front_dronedt/components/sections/FeaturedDrones.tsx
+++ front_dronedt/components/sections/FeaturedDrones.tsx
@@ -34,8 +34,7 @@ export default function FeaturesDrones() {
    
             <div 
        
-             verflow-hidden shadow-sm hover:shadow-2xl transition-all duration-500 border border-gray-200"
-            >
+             
               {/* Contenedor de Imagen */}
     
 
