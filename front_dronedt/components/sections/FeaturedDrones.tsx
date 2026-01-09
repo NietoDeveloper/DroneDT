6306dbc40a57427f1ff63d539f5518da3f919f80
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-08 23:54:10 -0500

    Update FeaturedDrones.tsx

--- front_dronedt/components/sections/FeaturedDrones.tsx
+++ front_dronedt/components/sections/FeaturedDrones.tsx
@@ -37,7 +37,7 @@ export default function FeaturesDrones() {
      
     
 
-                <div className="flex justify-between items-start mb-4">
+                <div className="flex 
                   <div>
                     <h3 className="text-2xl font-bold text-headingColor uppercase">{drone.name}</h3>
                     <p className="text-gray-600 mt-2 max-w-xs">{drone.description}</p>
