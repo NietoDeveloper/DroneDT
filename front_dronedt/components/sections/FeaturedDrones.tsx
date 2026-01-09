2f3abdf1ddf75e8be98fe14db0907ee079684c29
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-08 23:58:15 -0500

    Update FeaturedDrones.tsx

--- front_dronedt/components/sections/FeaturedDrones.tsx
+++ front_dronedt/components/sections/FeaturedDrones.tsx
@@ -3,11 +3,6 @@ import Link from 'next/link';
 
 // Simulación de datos (En el futuro esto vendrá de tu /back/api/drones)
 const DRONES_DATA = [
-  {
-    id: 'dt-01',
-    name: 'DT-Pro Phantom',
-    description: 'Precisión / Asegúrate de que este archivo exista
 
-  },
 
 
