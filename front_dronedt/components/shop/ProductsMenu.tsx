e7aa81ffa93f8c99dfdbb09108eb490811197c3f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-09 23:26:50 -0500

    Update ProductsMenu.tsx

--- front_dronedt/components/shop/ProductsMenu.tsx
+++ front_dronedt/components/shop/ProductsMenu.tsx
@@ -9,7 +9,7 @@ const ProductsMenu = () => {
       id: index + 1,
       name: `Drone Model ${index + 1}`,
       price: Math.floor(Math.random() * 500) + 100,
-      image: `https://via.placeholder.com/300x200?text, // Placeholder para imágenes
+      image: `https://, // Placeholder para imágenes
       description: 'High-performance drone wi
     }));
 
