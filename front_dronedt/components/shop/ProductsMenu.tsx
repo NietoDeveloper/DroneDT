5f44398f3d8cffaf198cda6961907cfe92a07e40
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-09 21:54:43 -0500

    Update ProductsMenu.tsx

--- front_dronedt/components/shop/ProductsMenu.tsx
+++ front_dronedt/components/shop/ProductsMenu.tsx
@@ -25,7 +25,7 @@ const ProductsMenu = () => {
     <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto px-4 py-8 max-w-[1900px] min-w-[310px]">
         <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 md:text-5xl lg:text-6xl">
-          Nuestros Drones
+
         </h1>
 
 
