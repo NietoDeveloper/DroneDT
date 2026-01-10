99f8246e8218669cd56764c232ceaec83b36df7a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-09 21:51:17 -0500

    Update ProductsMenu.tsx

--- front_dronedt/components/shop/ProductsMenu.tsx
+++ front_dronedt/components/shop/ProductsMenu.tsx
@@ -39,7 +39,7 @@ const ProductsMenu = () => {
           ))}
         </div>
       </div>
-      <div className="">
+      <>
         <button className="relative bg-green-600 text-white p-4 rounded-full shadow-lg hover:bg-green-700 transition duration-200">
           Carrito
           {cartCount > 0 && (
