ed2b4711a73688149571e38e30d0ff4e69cccdb0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-09 21:56:39 -0500

    Update ProductsMenu.tsx

--- front_dronedt/components/shop/ProductsMenu.tsx
+++ front_dronedt/components/shop/ProductsMenu.tsx
@@ -4,7 +4,6 @@ const ProductsMenu = () => {
   const [products, setProducts] = useState([]);
   const [cartCount, setCartCount] = useState(0);
 
-  // Simulación de
   useEffect(() => {tch real a tu API en Express/Mongo
     const dummyProducts = Array.from({ length: 20 }, (_, index) => ({
       id: index + 1,
