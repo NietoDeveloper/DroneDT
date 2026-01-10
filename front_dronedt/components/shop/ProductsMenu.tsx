f370ae5f26cac473e478a2ea73ca87f6a113470d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-09 21:54:48 -0500

    Update ProductsMenu.tsx

--- front_dronedt/components/shop/ProductsMenu.tsx
+++ front_dronedt/components/shop/ProductsMenu.tsx
@@ -23,7 +23,7 @@ const ProductsMenu = () => {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="container mx-auto px-4 py-8 max-w-[1900px] min-w-[310px]">
+      <div className="container my-8 max-w-[1900px] min-w-[310px]">
         <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 md:text-5xl lg:text-6xl">
 
         </h1>
