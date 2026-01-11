66f087b902c0a318c440fa1d75ddad889a1fda39
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-10 23:54:03 -0500

    Update VideoReel.tsx

--- front_dronedt/components/layout/VideoReel.tsx
+++ front_dronedt/components/layout/VideoReel.tsx
@@ -51,7 +51,7 @@ const DroneReel: React.FC = () => {
               >
                 Tu navegador no soporta el elemento de video.
               </video>
-            )}gray-600 text-lg">
+
             {videos[currentVideo]?.title}: {videos[currentVideo]?.description}
           </p>
               cla-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200"
