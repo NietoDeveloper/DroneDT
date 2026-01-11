a4bc64ee43568b44b04cf454180ade2fca19613c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-10 23:58:23 -0500

    Update VideoReel.tsx

--- front_dronedt/components/layout/VideoReel.tsx
+++ front_dronedt/components/layout/VideoReel.tsx
@@ -10,9 +10,6 @@ interface Video {
 
 setCurrentVideo] = useState<number>(0);
 
-  // Simulación de fetch de videos desde API (máximo, digamos, 10 videos para un reel)
-  useEffect(() => {
-
 
 
             ))}
