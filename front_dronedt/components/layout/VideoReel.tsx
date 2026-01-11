7a657243cd13c8e9404d961411a777c08ded7b6d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-10 23:59:24 -0500

    Update VideoReel.tsx

---
+++ front_dronedt/components/layout/VideoReel.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+interface Video {
+  id: number;
+  title: string;
+  videoUrl: string;
+  thumbnail: string;
+  description: string;
+}
+
+setCurrentVideo] = useState<number>(0);
+
+
+
+
+
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DroneReel;
