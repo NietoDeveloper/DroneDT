d234f54c8851cb60d8a06ad0e6483fc3458ac228
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-10 23:52:20 -0500

    Update VideoReel.tsx

--- front_dronedt/components/layout/VideoReel.tsx
+++ front_dronedt/components/layout/VideoReel.tsx
@@ -86,7 +86,6 @@ const DroneReel: React.FC = () => {
             ))}
           </div>
 
- er-blue-600' : ''}`}
                 onClick={() => setCurrentVideo(index)}
               />
             ))}
