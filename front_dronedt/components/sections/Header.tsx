69dced17563eedd5fef1b3f92bea699b0e42f8ee
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-11 23:55:03 -0500

    Update Header.tsx

--- front_dronedt/components/sections/Header.tsx
+++ front_dronedt/components/sections/Header.tsx
@@ -5,8 +5,4 @@ import Link from 'next/link';
 import { Menu, X, ShoppingCart, ChevronDown } from 'lucide-react';
 
 /**
- * Header Component - DroneDT
- * Ajustado para eliminar errores de importación y alineado al esqueleto del proyecto.
- */
-const Header: React.FC = () => {
 
