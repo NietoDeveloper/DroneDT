24a0deb5adeb792a9aed3934cbf5a994c14ca644
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-11 23:57:56 -0500

    Update Footer.tsx

--- front_dronedt/components/sections/Footer.tsx
+++ front_dronedt/components/sections/Footer.tsx
@@ -61,7 +61,6 @@ const Footer: FC = () => {
           <div className="flex space-x-6 mb-4 md:mb-0">
             <a href="https://twitter.com/dronedt" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors duration-200">X (Twitter)</a>
             <a href="https://facebook.com/dronedt" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors duration-200">Facebook</a>
-            <a href="https://instagram.com/dronedt" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors duration-200">Instagram</a>
           </div>
 
         </div>
