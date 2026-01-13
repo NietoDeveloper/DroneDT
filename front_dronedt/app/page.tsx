274d6fce03400e0eef4a8d241ea99f9419427d17
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 04:37:28 -0500

    Update page.tsx

--- front_dronedt/app/page.tsx
+++ front_dronedt/app/page.tsx
@@ -140,7 +140,7 @@ const Home: FC = () => {
       {/* Créditos finales de racha */}
       <div className="bg-white py-4 text-center opacity-20 hover:opacity-100 transition-opacity">
         <p className="text-[10px] font-black uppercase tracking-widest">
-          Build by Software DT | NietoDevelooper Streak: 152 Days
+          Build by Software DT | NietoDevelooper
         </p>
       </div>
     </div>
