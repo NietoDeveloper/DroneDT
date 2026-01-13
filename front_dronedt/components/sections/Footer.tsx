9b882d5b3100d9877eb0657cb1395e9aef82ad40
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-12 23:57:33 -0500

    Update Footer.tsx

--- front_dronedt/components/sections/Footer.tsx
+++ front_dronedt/components/sections/Footer.tsx
@@ -3,8 +3,3 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Bot, ShoppingBag } from 'lucide-react';
-
-const Footer: React.FC = () => {
-  const [time, setTime] = useState<string>("");
-
-
