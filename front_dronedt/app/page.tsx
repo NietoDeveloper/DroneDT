726b9cbf16fe51c0c7a7f42c09f49519e25c61ce
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 05:34:38 -0500

    Update page.tsx

--- front_dronedt/app/page.tsx
+++ front_dronedt/app/page.tsx
@@ -1,18 +1,4 @@
 
-              href="/demos" 
-              className="px-12 py-4 bg-transparent border-2 border-white text-white font-bold rounded-md uppercase text-xs tracking-[0.2em] transition-all hover:bg-white hover:text-black active:scale-95"
-            >
-              Agendar Demo
-            </Link>
-          </div>
-        </div>
-      </section>
-
-      {/* Créditos finales de racha */}
-      <div className="bg-white py-4 text-center opacity-20 hover:opacity-100 transition-opacity">
-        <p className="text-[10px] font-black uppercase tracking-widest">
-          Build by Software DT | NietoDeveloper
-        </p>
       </div>
     </div>
   );
