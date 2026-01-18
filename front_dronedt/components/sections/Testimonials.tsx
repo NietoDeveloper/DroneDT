f32ebfd209324f02ec752c48b01c1039c6a03656
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:39:40 -0500

    Update Testimonials.tsx

--- front_dronedt/components/sections/Testimonials.tsx
+++ front_dronedt/components/sections/Testimonials.tsx
@@ -1,20 +1,6 @@
 import { FC } from 'react';
 
 interface Testimonial {
-  name: string;
-  text: string;
-  rating: number;
-}
 
-interface TestimonialsProps {
-  title: string;
-  testimonials: Testimonial[];
-}
-
-        ))}
-      </div>
-    </section>
-  );
-};
 
 export default Testimonials;
