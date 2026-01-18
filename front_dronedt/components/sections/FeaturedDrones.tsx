d81cbc7d5473965493996a5e7786265f87c80d01
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:48:23 -0500

    Update FeaturedDrones.tsx

--- front_dronedt/components/sections/FeaturedDrones.tsx
+++ front_dronedt/components/sections/FeaturedDrones.tsx
@@ -11,23 +11,7 @@ export default function FeaturesDrones() {
           {DRONES_DATA.map((drone) => (
             <div 
               key={drone.id} 
-              className="group relative bg-card rounded-2xl overflow-hidden shadow-sm hover:shadow-2xl transition-all duration-500 border border-gray-200"
-            >
 
-
-                <div className="flex gap-4 mt-8">
-                  <Link 
-                    href={`/shop/drones/${drone.id}`}
-                    className="flex-1 bg-headingColor text-white text-center py-3 rounded-md font-bold hover:bg-opacity-90 transition-colors uppercase text-sm tracking-widest"
-                  >
-                    Explorar
-                  </Link>
-
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   );
