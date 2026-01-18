1bba4ab8cb1d69964c74bfdb07e15d6d83eb62ac
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:09:26 -0500

    Update tailwind.config.ts

--- front_dronedt/tailwind.config.ts
+++ front_dronedt/tailwind.config.ts
@@ -1,20 +1,5 @@
 import type { Config } from "tailwindcss";
 
-/** @type {import('tailwindcss').Config} */
-const config: Config = {
-  // Soporte para la estructura de última generación en la carpeta src
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      // Paleta de colores Software DT + DroneDT
-
-    },
-  },
-  plugins: [],
 };
 
 export default config;
