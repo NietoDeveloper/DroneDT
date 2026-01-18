7afb85d1685750527302f2c0b2499d4d9b420af2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:42:29 -0500

    Update layout.tsx

--- front_dronedt/app/layout.tsx
+++ front_dronedt/app/layout.tsx
@@ -10,11 +10,7 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   display: "swap",
 });
-
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-  display: "swap",
+lay: "swap",
 });
 
 export const metadata: Metadata = {
