35a49872324a9541cb71f4b36093a6fa5bc6cd79
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:46:10 -0500

    Update Features.tsx

--- front_dronedt/components/sections/Features.tsx
+++ front_dronedt/components/sections/Features.tsx
@@ -1,15 +1,3 @@
-import { FC } from 'react';
-
-interface Feature {
-  icon: string;
-  title: string;
-  desc: string;
-}
-
-interface FeaturesProps {
-  title: string;
-  features: Feature[];
-}
 
 
 export default Features;
