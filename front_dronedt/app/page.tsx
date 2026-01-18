51040cb8630f37d580232076c4c22ba517c8be1d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:50:01 -0500

    Update page.tsx

--- front_dronedt/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { FC } from 'react';
-import Link from 'next/link';
-import { ArrowRight, ChevronDown } from 'lucide-react';
-
-// Componentes secciones
-import DroneModels from '@/components/sections/DroneModels'; 
