348eee7fe7d0526b30900fb7f8714159512a8a37
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:48:55 -0500

    Update DroneModels.tsx

--- front_dronedt/components/sections/DroneModels.tsx
+++ front_dronedt/components/sections/DroneModels.tsx
@@ -1,20 +1,3 @@
 import { FC } from 'react';
 
-interface Drone {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-}
-
-interface DroneModelsProps {
-  title: string;
-  drones: Drone[];
-}
-
-
-  );
-};
-
 export default DroneModels;
