4a59d0caaef018cffd3640e9def869dc415f8473
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:09:18 -0500

    Update tailwind.config.ts

--- front_dronedt/tailwind.config.ts
+++ front_dronedt/tailwind.config.ts
@@ -12,14 +12,6 @@ const config: Config = {
     extend: {
       // Paleta de colores Software DT + DroneDT
 
-      // Sombras personalizadas para resaltar los drones
-      boxShadow: {
-        'drone-light': '0 4px 15px rgba(65, 105, 225, 0.1)',
-        'drone-gold': '0 4px 15px rgba(255, 215, 0, 0.3)',
-      },
-
-
-      }
     },
   },
   plugins: [],
