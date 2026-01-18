3ac8251c58aac7a9a9cea6e988d962c32ac85ced
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:09:05 -0500

    Update tailwind.config.ts

--- front_dronedt/tailwind.config.ts
+++ front_dronedt/tailwind.config.ts
@@ -21,11 +21,7 @@ const config: Config = {
       animation: {
         'float': 'hoverFloat 3s ease-in-out infinite',
       },
-      keyframes: {
-        hoverFloat: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
-        }
+
       }
     },
   },
