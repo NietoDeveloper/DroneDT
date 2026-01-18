f663bce0d7169f04ebf2787e4d84c456b59bb5e2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:19:13 -0500

    Update next.config.ts

--- front_dronedt/next.config.ts
+++ front_dronedt/next.config.ts
@@ -2,17 +2,3 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* 1. Activación del React Compiler */
-  /* Esto automatiza useMemo y useCallback para máximo rendimiento en DroneDT */
-  experimental: {
-    reactCompiler: true,
-  },
-
-  /* 3. Configuración de Seguridad y Limpieza */
-  poweredByHeader: false, // Oculta que la app corre con Next.js (Seguridad)
-  reactStrictMode: true,  // Ayuda a detectar renderizados dobles en desarrollo
-
-  /* 4. Alias de Rutas y Webpack (Opcional, Next lo maneja por defecto con el alias @/*) */
-
-
-export default nextConfig;
