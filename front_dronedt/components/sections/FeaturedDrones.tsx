313a0f621e1792a7b6272b932bcfd1e6e4b51e2f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 22:48:29 -0500

    Update FeaturedDrones.tsx

--- front_dronedt/components/sections/FeaturedDrones.tsx
+++ front_dronedt/components/sections/FeaturedDrones.tsx
@@ -1,18 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-
-export default function FeaturesDrones() {
-  return (
-    <section className="bg-main py-24 px-6 md:px-12">
-      <div className="max-w-7xl mx-auto">
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          {DRONES_DATA.map((drone) => (
-            <div 
-              key={drone.id} 
-
-      </div>
     </section>
   );
 }
