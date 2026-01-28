ac8dc961ea475b8f30a604a3a213b54ac9ee1a1e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-28 02:59:22 -0500

    eslint

--- front_dronedt/eslint.config.mjs
+++ front_dronedt/eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  basePath: __dirname,
+});
+
+const eslintConfig = [
+  // 1. Heredamos las reglas de Next de forma compatible
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  // 2. Definimos los archivos a ignorar (Equivalente a globalIgnores)
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "node_modules/**"
+    ],
+  },
+];
 
 export default eslintConfig;
