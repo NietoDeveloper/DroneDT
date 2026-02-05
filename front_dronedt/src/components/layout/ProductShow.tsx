148939152306e0ac23b6fcbd9e7d44daaf74918d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-04 23:46:33 -0500

    Update ProductShow.tsx

--- front_dronedt/src/components/layout/ProductShow.tsx
+++ front_dronedt/src/components/layout/ProductShow.tsx
@@ -3,16 +3,6 @@
 import React, { useRef, useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 
-interface Product {
-  _id: string;
-  name: string;
-  brand?: string; // Opcional por si no viene de Atlas
-  description: string;
-  price: number;
-  // Ajustado para aceptar string[] (como lo pusimos en Atlas) o el objeto previo
-  images: any[]; 
-  category: any;
-}
 
 
 
