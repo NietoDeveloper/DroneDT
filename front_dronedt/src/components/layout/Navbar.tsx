d3f98d741a48f05e0ba43c43689a39d21e32098b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-06 23:49:56 -0500

    Update Navbar.tsx

--- front_dronedt/src/components/layout/Navbar.tsx
+++ front_dronedt/src/components/layout/Navbar.tsx
@@ -178,13 +178,7 @@ const Navbar = () => {
               ))}
             </div>
           ) : (
-             <div className="pt-10">
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pb-20">
-                   {menuContent[selectedModel]?.map((product) => (
-
-             </div>
-          )}
+   
         </div>
       </div>
     </>
