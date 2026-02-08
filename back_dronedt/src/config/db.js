5b3f4393d9b427646ad163f45b2c058220fc459e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-07 23:50:28 -0500

    Update db.js

---
+++ back_dronedt/src/config/db.js
@@ -0,0 +1,16 @@
+const { assetsConnection } = require('../config/db');
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: String,
+    image: String,
+    stock: { type: Number, default: 0 }
+}, { timestamps: true });
+
+// IMPORTANTE: 
+// 1. Usamos assetsConnection (Clúster 2)
+// 2. Forzamos el nombre de la colección 'products' para evitar fallos de pluralización
+module.exports = assetsConnection.model('Product', productSchema, 'products');
