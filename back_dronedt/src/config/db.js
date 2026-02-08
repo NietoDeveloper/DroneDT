07969e82c5d2592d89e3a238943229a40f8c1bd7
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-07 23:49:37 -0500

    Update db.js

--- back_dronedt/src/config/db.js
+++ back_dronedt/src/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 /**
  * Drone DT - Enterprise Multi-Cluster Manager
- * Solución de alta disponibilidad optimizada para el Committer #1.
- * Inicialización inmediata para evitar errores de 'undefined' en los modelos.
- */
+
 
 // Inicializamos las instancias de conexión de inmediato como objetos de Mongoose
 const coreConnection = mongoose.createConnection();
