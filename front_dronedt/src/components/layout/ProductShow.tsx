3b564358466a19d33ac7d9a7cfb35b14434989b4
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-09 23:44:17 -0500

    Update ProductShow.tsx

--- front_dronedt/src/components/layout/ProductShow.tsx
+++ front_dronedt/src/components/layout/ProductShow.tsx
@@ -1,12 +1,3 @@
-"use client";
-
-import React, { useRef, useState, useEffect, useCallback } from 'react';
-import { useRouter } from 'next/navigation';
-
-interface Category {
-  _id: string;
-  name: string;
-}
 
 
 export default ProductShow;
