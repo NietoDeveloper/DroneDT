ffa2043a59558cee7a47eff88d74cd68e1779d55
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-09 23:48:57 -0500

    Update Navbar.tsx

--- front_dronedt/src/components/layout/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useState, useEffect, useCallback } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { X, ChevronRight, Circle } from 'lucide-react';
-
