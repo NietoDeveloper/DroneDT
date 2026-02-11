fd09e561b6b5fbb2862139ef63d0f1b3ed518319
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-11 18:27:20 -0500

    Update next.config.ts

--- front_dronedt/next.config.ts
+++ front_dronedt/next.config.ts
@@ -13,9 +13,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+
 
   /* Output optimizado para Vercel y Docker */
   output: 'standalone',
