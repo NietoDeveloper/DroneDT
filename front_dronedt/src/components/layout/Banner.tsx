739955dc77bca512094f9771c00c8664b24eb9fd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-10 19:43:18 -0500

    Update Banner.tsx

--- front_dronedt/src/components/layout/Banner.tsx
+++ front_dronedt/src/components/layout/Banner.tsx
@@ -101,9 +101,6 @@ const Banner = () => {
             className="animate-in fade-in slide-in-from-top duration-1000 w-full"
           >
 
-            <p className="text-white text-[10px] md:text-[11px] tracking-[0.5em] uppercase font-bold drop-shadow-md mt-4 opacity-90">
-
-            </p>
           </div>
         </div>
 
