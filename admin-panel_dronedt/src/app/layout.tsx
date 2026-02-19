f479c891fd5725aa2c1adf634e55f3509fc4aff8
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-19 14:34:30 -0500

    Update layout.tsx

--- admin-panel_dronedt/src/app/layout.tsx
+++ admin-panel_dronedt/src/app/layout.tsx
@@ -1,13 +1,4 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "Drone DT | Admin Control Panel",
-  description: "World-class drone management system by NietoDeveloper",
-};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
