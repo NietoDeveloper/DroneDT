23008a3691b339aab27a10773045f0eae689f706
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-28 20:02:20 -0500

    Delete page.tsx

--- front_dronedt/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";{`
-        html, body { overflow: hidden; height: 100%; margin: 0; background: #DCDCDC; }
-        .custom-scrollbar::-webkit-scrollbar { width: 6px; }
-        .custom-scrollbar::-webkit-scrollbar-track { background: #000; }
-        .custom-scrollbar::-webkit-scrollbar-thumb { background: #FFD700; border-radius: 10px; }
-      `}</style>
-    </div>
-  );
-}
