5534a9eeccdfcca7c027139b4eeb384ae67e9111
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-30 13:30:38 -0500

    Update next.config.ts

--- Front-DroneDT/next.config.ts
+++ Front-DroneDT/next.config.ts
@@ -5,14 +5,5 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   compress: true,
   
-  // 🛡️ Arquitectura de Seguridad y Despliegue
-  output: 'standalone', // Ideal para Docker y Railway
-  
-  typescript: {
-    ignoreBuildErrors: false, // Mantenemos la integridad del tipado en producción
-  },
-
-
-
 
 export default nextConfig;
