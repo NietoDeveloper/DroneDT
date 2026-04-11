fd22e92643d5e433581a8750da8751ccc3c1a2fb
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-10 23:38:06 -0500

    Update GalleryShowcase.tsx

--- Front-DroneDT/src/components/layout/GalleryShowcase.tsx
+++ Front-DroneDT/src/components/layout/GalleryShowcase.tsx
@@ -106,10 +106,6 @@ export const GalleryShowcase: React.FC = () => {
             </p>
           </div>
 
-          <button className="relative flex items-center gap-6 text-[#000000] font-black uppercase tracking-[.25em] group text-sm">
-            <span className="relative z-10 group-hover:text-[#FEB60D] transition-colors duration-300">
-              Explorar Flota Completa
-            </span>
             <div className="relative w-14 h-14 flex items-center justify-center transition-all duration-500 group-hover:rotate-45">
                <svg width="40" height="40" viewBox="0 0 100 100" className="absolute">
                   <circle cx="50" cy="50" r="48" stroke="currentColor" strokeWidth="1" fill="none" strokeDasharray="10 10" />
