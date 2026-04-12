fffa770ab3ef2c443dba3025fb9ec11b4afc5023
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-11 23:47:46 -0500

    Update GalleryShowcase.tsx

--- Front-DroneDT/src/components/layout/GalleryShowcase.tsx
+++ Front-DroneDT/src/components/layout/GalleryShowcase.tsx
@@ -2,15 +2,6 @@
 
 import React, { useState, useEffect, useCallback } from 'react';
 
-/**
- * ARCHITECT: Manuel Nieto | Nieto Laboratory
- * COMPONENT: GalleryShowcase v1.5 - Remote Assets Ready
- * UPDATE: Conexión de videos online y placeholders de alta resolución
- */
-
-interface GalleryItem {
-  id: string;
-
 
 
 export default GalleryShowcase;
