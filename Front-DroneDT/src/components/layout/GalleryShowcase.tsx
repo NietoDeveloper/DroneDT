d02e2e873e1a1fcb98ce8436f374b01312aa2f35
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-11 23:40:02 -0500

    Update GalleryShowcase.tsx

--- Front-DroneDT/src/components/layout/GalleryShowcase.tsx
+++ Front-DroneDT/src/components/layout/GalleryShowcase.tsx
@@ -95,8 +95,7 @@ export const GalleryShowcase: React.FC = () => {
     
        
 
-      
-              <div className="absolute inset-0 z-20 p-8 flex flex-col justify-end">
+      absolute inset-0 z-20 p-8 flex flex-col justify-end">
                 <div className="translate-y-8 group-hover:translate-y-0 transition-transform duration-500">
                   <span className="inline-block bg-[#FFD700] text-black text-[9px] font-black tracking-widest uppercase px-3 py-1 mb-3">
                     {item.category}
