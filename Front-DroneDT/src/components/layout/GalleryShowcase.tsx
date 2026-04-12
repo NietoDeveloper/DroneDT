3418bd9a40b4e2825524df35f8e81d2c34b1cf5b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-11 23:47:37 -0500

    Update GalleryShowcase.tsx

--- Front-DroneDT/src/components/layout/GalleryShowcase.tsx
+++ Front-DroneDT/src/components/layout/GalleryShowcase.tsx
@@ -10,14 +10,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 interface GalleryItem {
   id: string;
-  type: 'image' | 'video';
-  url: string;
-  title: string;
-  category: string;
-  span?: string; 
-}
-
-
 
 
 
