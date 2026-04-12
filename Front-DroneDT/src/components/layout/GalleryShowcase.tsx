f53948729c312b6c5c3e6c90dd78ad329fd6498e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-11 23:39:21 -0500

    Update GalleryShowcase.tsx

--- Front-DroneDT/src/components/layout/GalleryShowcase.tsx
+++ Front-DroneDT/src/components/layout/GalleryShowcase.tsx
@@ -102,7 +102,6 @@ export const GalleryShowcase: React.FC = () => {
                   </video>
                 ) :
 
-              {/* OVERLAY DE GRADIENTE */}
               <div className="absolute inset-0 bg-gradient-to-t from-black via-black/20 to-transparent opacity-80 group-hover:opacity-60 transition-opacity duration-500 z-10" />
 
               {/* INFO DEL ITEM */}
