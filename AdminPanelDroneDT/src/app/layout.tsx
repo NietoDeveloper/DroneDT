87dad7c76afe8da4c93e5e500c939932e82dc769
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-29 23:55:31 -0500

    Update layout.tsx

--- AdminPanelDroneDT/src/app/layout.tsx
+++ AdminPanelDroneDT/src/app/layout.tsx
@@ -2,12 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css"; 
 import Preloader from "@/components/ui/Preloader";
 import Logo from "@/components/ui/Logo";
-xport default f
-                  
+xport defaults        
       
                        
-                   
- 
-    </html>
   );
 }
