bfe0128a33e6b7aa9355f2f22654be70c9a30371
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-30 23:29:47 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -32,8 +32,7 @@ export default function LoginPage() {
           backgroundSize: '40px 40px' 
         }} 
       />
-      
-      {/* Brillo ambiental Drone DT */}
+
       <div className="absolute top-[-20%] left-[-10%] w-[50%] h-[50%] bg-gold/5 blur-[120px] rounded-full pointer-events-none" />
 
       <div className="max-w-md w-full z-10">
