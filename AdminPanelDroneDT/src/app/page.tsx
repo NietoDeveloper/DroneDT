2d5601d48e87db71d0d4592a37c7569644a2a533
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-30 23:47:33 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -2,9 +2,6 @@
 
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Logo from '@/components/ui/Logo';
-
-/** * LoginPage - Drone DT Intelligence System
- * Engineered by: NietoDeveloper (Top 1 Colombia)
+import Logo from '@/compone1 Colombia)
  */
 
