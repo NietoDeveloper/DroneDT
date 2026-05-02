78a61e6acf2ba543fc462ebbd0a85a54b2a15992
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-01 22:32:25 -0500

    Update layout.tsx

--- AdminPanelDroneDT/src/app/(dashboard)/layout.tsx
+++ AdminPanelDroneDT/src/app/(dashboard)/layout.tsx
@@ -5,12 +5,6 @@ import { useDashboardStore } from '@/store/useDashboardStore';
 import { 
   LayoutDashboard, 
   Package, 
-
-}
-
-export default function DashboardLayout({ children }: LayoutProps) {
-  const { isSidebarOpen, toggleSidebar, userProfile } = useDashboardStore();
-
   const navigation = [
 
 
