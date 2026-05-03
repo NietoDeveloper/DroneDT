33c6074450ef78dccef363ae7e5a1a2bc878db1e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-02 23:39:31 -0500

    Update tailwind.config.ts

--- AdminPanelDroneDT/tailwind.config.ts
+++ AdminPanelDroneDT/tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   // Escaneo profundo: cubrimos todas las capas de la arquitectura monorepo
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+
 
 
 export default config;
