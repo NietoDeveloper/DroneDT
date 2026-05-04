154851b9d8b1afc7623c2607637d4454ab620d6b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-03 23:33:54 -0500

    Update next.config.mjs

--- AdminPanelDroneDT/next.config.mjs
+++ AdminPanelDroneDT/next.config.mjs
@@ -1,10 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* 
-     Habilita el modo estricto de React para detectar efectos secundarios 
-     y problemas de renderizado en el ciclo S+.
-  */
-  reactStrictMode: true,
+
 
 
 export default nextConfig;
