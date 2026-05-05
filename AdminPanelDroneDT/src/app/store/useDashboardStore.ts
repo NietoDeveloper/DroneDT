93fe392050dab40b98d66f2b473213c23d1a97de
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-04 23:49:47 -0500

    Update useDashboardStore.ts

--- AdminPanelDroneDT/src/app/store/useDashboardStore.ts
+++ AdminPanelDroneDT/src/app/store/useDashboardStore.ts
@@ -2,9 +2,7 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface UserProfile {
-  name: string;
-  role: string;
-  avatar?: string;
+
 
   )
 );
