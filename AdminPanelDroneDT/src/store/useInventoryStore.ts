8affa8841ec5b013b632379067358d4dbbd45e05
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-05 22:16:13 -0500

    Update useInventoryStore.ts

--- AdminPanelDroneDT/src/store/useInventoryStore.ts
+++ AdminPanelDroneDT/src/store/useInventoryStore.ts
@@ -1,13 +1,4 @@
 import { create } from 'zustand';
-
-// Estructura de nivel L5 para Esmeraldas y Activos
-export interface Product {
-  id: string;
-  sku: string;         // Identificador único (ej: EM-001-2026)
-  name: string;
-  carats: number;      // Peso en quilates
-  price: number;       // Precio en USD
-  status: 'AVAILABLE' | 'SOLD' | 'RESERVED';
-  lastUpdate: string;  // Timestamp para el Live Sync
+astUpdate: string;  // Timestamp para el Live Sync
 }
 
