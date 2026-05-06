46d4e4994e0a766a840d81a93bf9f07d8078c604
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-05 22:59:27 -0500

    Update InventoryGrid.tsx

--- AdminPanelDroneDT/src/components/dashboard/InventoryGrid.tsx
+++ AdminPanelDroneDT/src/components/dashboard/InventoryGrid.tsx
@@ -2,8 +2,3 @@
 
 import React from 'react';
 import { useInventoryStore } from '@/store/useInventoryStore';
-import { InventoryCard } from './InventoryCard'; // Lo crearemos a continuación
-import { PackageSearch, Loader2 } from 'lucide-react';
-
-
-};
