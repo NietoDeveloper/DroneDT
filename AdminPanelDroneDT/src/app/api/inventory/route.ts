84e0d33a3c836b91dbdf27368a61e4055f39b002
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-06 23:33:25 -0500

    Update route.ts

--- AdminPanelDroneDT/src/app/api/inventory/route.ts
+++ AdminPanelDroneDT/src/app/api/inventory/route.ts
@@ -2,8 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   // Datos temporales para visualizar el grid L5
-  const mockData = Array.from({ length: 12 }).map((_, i) => ({
-    id: `EM-${1000 + i}`,
+
   }));
 
   return NextResponse.json(mockData);
