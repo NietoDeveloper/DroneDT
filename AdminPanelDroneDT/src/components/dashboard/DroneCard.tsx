dd6f171aff1c226550fe0695295de6a54e041672
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-07 22:44:39 -0500

    Update DroneCard.tsx

--- AdminPanelDroneDT/src/components/dashboard/DroneCard.tsx
+++ AdminPanelDroneDT/src/components/dashboard/DroneCard.tsx
@@ -14,6 +14,6 @@ import {
 }
 
 export const DroneCard = memo(({ drone }: DroneCardProps) => {
-  // Lógica de criticidad de nivel L5
+
   const isCritical = drone.battery < 20 || drone.status === 'CRITICAL';
   const isFlying = drone.status === 'FLYING';
