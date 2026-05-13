0371f9883ee63134bfce7af1ba7e8bbd697774bf
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-12 23:53:51 -0500

    Update Sidebar.tsx

--- AdminPanelDroneDT/src/components/dashboard/dashboard/Sidebar.tsx
+++ AdminPanelDroneDT/src/components/dashboard/dashboard/Sidebar.tsx
@@ -19,5 +19,5 @@ export const Sidebar = () => {
     { name: 'CONFIG', icon: <Settings size={20} />, path: '/dashboard/settings' },
   ];
 
-  return (:w-[240px] bg-black border-r border-white/10 flex flex-col z-50">
+  return (:w-[240px] bg-black border-rcol z-50">
       {/* BRANDING */}t-white uppercase">
