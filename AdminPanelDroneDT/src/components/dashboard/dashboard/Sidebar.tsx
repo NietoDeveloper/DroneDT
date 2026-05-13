4e8b831cb68d971aa028023b7d0d92761f7ccc0e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-12 23:51:08 -0500

    Update Sidebar.tsx

--- AdminPanelDroneDT/src/components/dashboard/dashboard/Sidebar.tsx
+++ AdminPanelDroneDT/src/components/dashboard/dashboard/Sidebar.tsx
@@ -50,9 +50,7 @@ export const Sidebar = () => {
               <span className="hidden lg:block ml-4 text-[11px] font-black tracking-[0.1em] uppercase">
                 {item.name}
               </span>
-              
-              {/* Tooltip para mobile */}
-              <iv className="lg:hidden absolute left-14 bg-emerald-500 text-black text-[10px] font-bold px-2 py-1 rounded opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity whitespace-nowrap">
+              merald-500 text-black text-[10px] font-bold px-2 py-1 rounded opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity whitespace-nowrap">
                 {item.name}
 
   );
