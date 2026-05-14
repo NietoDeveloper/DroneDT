aa13126c6105b9e9210d9f3b52600f7cf366512a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-13 19:28:01 -0500

    Update useUserManagement.ts

--- AdminPanelDroneDT/src/hooks/useUserManagement.ts
+++ AdminPanelDroneDT/src/hooks/useUserManagement.ts
@@ -41,8 +41,6 @@ export const useUserManagement = (autoSyncInterval = 30000) => {
         },
         {
           _id: 'OP-002',
-          name: 'MONICA R.',
-          email: 'monica@nieto-lab.com',
 
         },
 
