021a1a24bac10d6f79cf9042a309875658cfb20d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-14 23:31:44 -0500

    Update useUserStore.ts

--- AdminPanelDroneDT/src/store/useUserStore.ts
+++ AdminPanelDroneDT/src/store/useUserStore.ts
@@ -10,6 +10,3 @@ export interface Operator {
   name: string;
   email: string;
   role: 'ADMIN_CHIEF' | 'FIELD_OPERATOR' | 'TECH_ENGINEER' | 'SECURITY_OFFICER';
-  status: 'ACTIVE' | 'IDLE' | 'OFFLINE' | 'SUSPENDED';
-  location: string;
-  lastUplink: string;
