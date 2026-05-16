8dfc8667249e135235ed930b627f055276acd93a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-15 20:03:13 -0500

    Update useUserStore.ts

--- AdminPanelDroneDT/src/store/useUserStore.ts
+++ AdminPanelDroneDT/src/store/useUserStore.ts
@@ -2,11 +2,7 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 /**
- * L5_OPERATOR_ENTITY
- * Estructura de Grado Industrial para Drone DT
- */
-export interface Operator {
-  _id: string;
+
 
 
   
