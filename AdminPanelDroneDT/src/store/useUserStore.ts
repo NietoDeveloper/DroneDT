c867817dbcb1a387d9da6703ce2182d8a3d6acc5
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-16 16:38:38 -0500

    Update useUserStore.ts

--- AdminPanelDroneDT/src/store/useUserStore.ts
+++ AdminPanelDroneDT/src/store/useUserStore.ts
@@ -121,8 +121,6 @@ export const useUserStore = create<UserState>()(
   )
 );
 
-/**
- * SELECTORES L5 - Optimización de Memoria
 
 export const selectActiveOperators = (state: UserState) => 
   state.operators.filter(op => op.status === 'ACTIVE');
