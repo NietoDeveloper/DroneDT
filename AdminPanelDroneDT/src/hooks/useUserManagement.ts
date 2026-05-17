b6c10ea7813c2b2dfa17c0781fd726f3fe783d08
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-16 23:54:24 -0500

    Update useUserManagement.ts

--- AdminPanelDroneDT/src/hooks/useUserManagement.ts
+++ AdminPanelDroneDT/src/hooks/useUserManagement.ts
@@ -75,6 +75,3 @@ export const useUserManagement = (autoSyncInterval = 30000) => {
   useEffect(() => {
     setIsReady(true); // El componente ya está montado en el cliente
     fetchOperators();
-
-  
- 
