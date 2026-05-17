ee441dd081c7d4339a66b7d0fe9041540af7a597
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-16 23:55:04 -0500

    Update useUserManagement.ts

--- AdminPanelDroneDT/src/hooks/useUserManagement.ts
+++ AdminPanelDroneDT/src/hooks/useUserManagement.ts
@@ -13,8 +13,7 @@ export const useUserManagement = (autoSyncInterval = 30000) => {
   
   const { 
     setOperators, 
-    setLoadi
-    operators,
+    se
     isLoading: storeLoading 
   } = useUserStore();
   
