1e8c0d12f28ac7fd67d956f63e7a50733549c191
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-17 23:50:59 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -123,7 +123,7 @@ export default function UsersPage() {
           </span>
           <span className="hidden sm:inline">UPLINK: ACTIVE</span>
         </div>
-        <div className="flex items-center gap-4">s group"form duration-500'}`} />
+        <div className="flex items-center gap-4">s group"form duion-500'}`} />
             REFRESH_SYSTEM
           </button>
     
