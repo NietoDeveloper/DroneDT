d74df15685313863792e8a0902e41f34a8a5f129
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-19 23:48:10 -0500

    Update Preloader.tsx

--- AdminPanelDroneDT/src/components/ui/Preloader.tsx
+++ AdminPanelDroneDT/src/components/ui/Preloader.tsx
@@ -73,7 +73,6 @@ export default function Preloader() {
             animate={{ opacity: 1 }}
             className="mt-24 text-center space-y-6"
           >
-            <div>[0.5em] uppercase mt-2 font-mono">Autonomous Intellgine</p>
             </div>
 
             </div>
