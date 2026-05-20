977412cf939eb775addb41d1ec51bfedf9a9f32a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-19 23:48:47 -0500

    Update Preloader.tsx

--- AdminPanelDroneDT/src/components/ui/Preloader.tsx
+++ AdminPanelDroneDT/src/components/ui/Preloader.tsx
@@ -60,8 +60,7 @@ export default function Preloader() {
                       animate={{ rotate: 360 }}
                       transition={{ repeat: Infinity, duration: 0.1, ease: "linear" }} // RPMs aumentadas
                       className="w-12 h-12 border-t-2 border-b-[1px] border-[#FFD700]/50 rounded-full blur-[0.8px]"
-                    />
-                    <div className="absolute w-3 h-3 bg-zinc-700 rounded-full border border-zinc-600" />
+                    />rounded-full border border-zinc-600" />
                 </div>
               </div>
             ))}
