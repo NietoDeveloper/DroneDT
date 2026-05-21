5101a6b80f9d25185c821cd0ff3ea47bc136b58a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-21 12:02:51 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -4,12 +4,4 @@ import React, { useState } from 'react';
 import { UserStats } from '@/components/users/UserStats';
 import { UserRow } from '@/components/users/UserRow';
 import { useUserManagement } from '@/hooks/useUserManagement';
-import { Preloader } from '@/components/ui/Preloader'; // Asegúrate que esta ruta sea correcta
-import { 
-  Search, 
-  RefreshCw, 
-  UserPlus, 
-  ShieldAlert,
-  Zap,
-  Activity
-} from 'lucide-react';
+
