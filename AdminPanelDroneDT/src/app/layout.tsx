0b2ba196b1b2ef3bcd595d2fdcc1c071299c5729
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-21 11:40:55 -0500

    Update layout.tsx

--- AdminPanelDroneDT/src/app/layout.tsx
+++ AdminPanelDroneDT/src/app/layout.tsx
@@ -7,7 +7,4 @@ import { Preloader } from "@/components/ui/Preloader";
  * Metadata - Drone DT Intelligence System
  * World-Class Engineering by NietoDeveloper
  */
-export const metadata: Metadata = {
-  title: {
-    template: '%s | Panel Control Empresa Drone DT',
-    default: 'Panel Control Empresa DroneDT',
+exp
