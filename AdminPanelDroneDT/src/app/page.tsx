57c56f2b07de22c62682a0fe64a9b9037fe3a642
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-21 12:00:19 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -143,7 +143,7 @@ export default function UsersPage() {
         .custom-scrollbar::-webkit-scrollbar { width: 4px; }
         .custom-scrollbar::-webkit-scrollbar-track { background: rgba(255, 255, 255, 0.02); }
         .custom-scrollbar::-webkit-scrollbar-thumb { 
-          background: rgba(16, 185, 129, 0.2); 
+
           border-radius: 10px; 
         }
         .custom-scrollbar::-webkit-scrollbar-thumb:hover { background: rgba(16, 185, 129, 0.5); }
