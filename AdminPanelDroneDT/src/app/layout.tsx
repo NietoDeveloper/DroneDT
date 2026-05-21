8e54340caf7deadb940c53cc25ee3ef1853287bd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-21 11:41:01 -0500

    Update layout.tsx

--- AdminPanelDroneDT/src/app/layout.tsx
+++ AdminPanelDroneDT/src/app/layout.tsx
@@ -1,10 +1,2 @@
 import type { Metadata } from "next";
 import "./globals.css"; 
-// CORRECCIÓN: Importación nombrada usando llaves para machacar el error de Turbopack
-import { Preloader } from "@/components/ui/Preloader";
-
-/**
- * Metadata - Drone DT Intelligence System
- * World-Class Engineering by NietoDeveloper
- */
-exp
