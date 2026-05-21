91250f806ed0de8baf7dd1d62ad098d29e39caff
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-20 21:39:52 -0500

    Update layout.tsx

--- AdminPanelDroneDT/src/app/layout.tsx
+++ AdminPanelDroneDT/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css"; 
-import Preloader from "@/components/ui/Preload
-
-/**
+import Preloader from "@/components/ui/Preloa
  * Metadata - Drone DT Intelligen
  * World-Class Engin
 export const metad
