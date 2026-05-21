ffb6045f33fe575857f0745532a87bce2c1f2090
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-20 21:34:30 -0500

    Update layout.tsx

--- AdminPanelDroneDT/src/app/layout.tsx
+++ AdminPanelDroneDT/src/app/layout.tsx
@@ -40,10 +40,6 @@ export default function RootLayout({
         {/* Barra superior dorada Aeroespacial - Línea de identidad Drone DT */}
         <div className="fixed top-0 left-0 right-0 h-[2px] bg-gradient-to-r from-gold via-yellow-color to-gold z-[9999]" />
 
-        {/* 
-
-
-        */}
         <main className="flex-1 relative z-10 h-full w-full overflow-hidden">
           {children}
         </main>
