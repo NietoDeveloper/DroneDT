86663a3836fa7d0b0e423a27b93b069a7ffc2f75
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-21 12:02:59 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -1,7 +1,1 @@
-'use client';
-
-import React, { useState } from 'react';
-import { UserStats } from '@/components/users/UserStats';
-import { UserRow } from '@/components/users/UserRow';
-import { useUserManagement } from '@/hooks/useUserManagement';
 
