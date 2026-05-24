57599b7d567f41bbb29a16cb249fb0185e075adc
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-05-23 21:59:40 -0500

    Update page.tsx

--- AdminPanelDroneDT/src/app/page.tsx
+++ AdminPanelDroneDT/src/app/page.tsx
@@ -1,15 +1,4 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { Preloader } from '@/components/ui/Preloader';
-import { motion, AnimatePresence } from 'framer-motion';sponibles)
-  const productMetrics = {
-      const now = new Date();
-      setSystemTime(now.toTimeString().split(' ')[0]);
-    }, 1000);
-
-
-      <AnimatePresence mode="wait">
-        {/* INTERFAZ 1: GATEWAY DE INGRESO (BYPASS) */}
-        {currentFlow === 'LOGIN' && (
-          <motion.div
+import { Preloader } from '@/components/ui/Preload
